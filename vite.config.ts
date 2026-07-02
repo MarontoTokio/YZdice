87605742ca6e9feec293fb578640c1cd0e06b70d
MarontoTokio <MarontoTokio@example.com>
2026-07-02 12:23:01 +0600

    Fix: update base path for plugin

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ declare var __dirname: string;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [react()],
   assetsInclude: ["**/*.glb", "**/*.hdr"],
   build: {
